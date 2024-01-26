b19ac53581cc2c0c8a4cae23c4588e2b97270c64
YuriWit <yuridewitherran@gmail.com>
2024-01-26 11:51:30 -0300

    input almousr working

--- main.ts
+++ main.ts
@@ -16,66 +16,71 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('file-input', 'XML to Markdown', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
+			new SampleModal(this.app, (result) => {
+				new Notice(`Hello, ${result}!`);
+			  }).open();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// // Perform additional things with the ribbon
+		// ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		// // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText('Status Bar Text');
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		// // This adds a simple command that can be triggered anywhere
+		// this.addCommand({
+		// 	id: 'open-sample-modal-simple',
+		// 	name: 'Open sample modal (simple)',
+		// 	callback: () => {
+		// 		new SampleModal(this.app).open();
+		// 	}
+		// });
+
+		// // This adds an editor command that can perform some operation on the current editor instance
+		// this.addCommand({
+		// 	id: 'sample-editor-command',
+		// 	name: 'Sample editor command',
+		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
+		// 		console.log(editor.getSelection());
+		// 		editor.replaceSelection('Sample Editor Command');
+		// 	}
+		// });
+
+		// // This adds a complex command that can check whether the current state of the app allows execution of the command
+		// this.addCommand({
+		// 	id: 'open-sample-modal-complex',
+		// 	name: 'Open sample modal (complex)',
+		// 	checkCallback: (checking: boolean) => {
+		// 		// Conditions to check
+		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 		if (markdownView) {
+		// 			// If checking is true, we're simply "checking" if the command can be run.
+		// 			// If checking is false, then we want to actually perform the operation.
+		// 			if (!checking) {
+		// 				new SampleModal(this.app).open();
+		// 			}
+
+		// 			// This command will only show up in Command Palette when the check function returns true
+		// 			return true;
+		// 		}
+		// 	}
+		// });
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// // Using this function will automatically remove the event listener when this plugin is disabled.
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -92,13 +97,79 @@ export default class MyPlugin extends Plugin {
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
+	result: string;
+	onSubmit: (result: string) => void;
+
+	constructor(app: App, onSubmit: (result: string) => void) {
 		super(app);
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		contentEl.empty();
+
+		new Setting(contentEl)
+            .setHeading()
+            .setName("Import XML Data");
+
+        const importSettingsContainer = contentEl.createDiv(
+            "statblock-additional-container"
+        );
+
+		new Setting(importSettingsContainer).setDesc(
+            "Import data from xml files. Monsters are stored by name, so only the last creature by that name will be saved. This is destructive - any saved creature will be overwritten."
+        );
+
+		const importAdditional =
+			importSettingsContainer.createDiv("additional");
+
+		const importAppFile = new Setting(importAdditional)
+            .setName("Import DnDAppFile")
+            .setDesc("Only import content that you own.");
+
+		const inputAppFile = createEl("input", {
+			attr: {
+				type: "file",
+				name: "dndappfile",
+				accept: ".xml",
+				multiple: true
+			}
+		});
+
+		inputAppFile.onchange = async () => {
+            const { files } = inputAppFile;
+            if (!files.length) return;
+            try {
+                const { files } = inputAppFile;
+                if (!files.length) return;
+                const monsters = await this.importer.import(files, "appfile");
+                if (monsters && monsters.length) {
+                    await this.plugin.saveMonsters(monsters);
+                }
+                this.display();
+            } catch (e) {}
+        };
+
+		importAppFile.addButton((b) => {
+            b.setButtonText("Choose File(s)").setTooltip(
+                "Import DnDAppFile Data"
+            );
+            b.buttonEl.addClass("statblock-file-upload");
+            b.buttonEl.appendChild(inputAppFile);
+            b.onClick(() => inputAppFile.click());
+        });
+
+		new Setting(contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						this.close();
+						this.onSubmit(this.result);
+					}));
 	}
 
 	onClose() {
@@ -121,14 +192,54 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+            .setHeading()
+            .setName("Import Homebrew Creatures");
+
+        const importSettingsContainer = containerEl.createDiv(
+            "statblock-additional-container"
+        );
+
+		new Setting(importSettingsContainer).setDesc(
+            "Import creatures from creature files. Monsters are stored by name, so only the last creature by that name will be saved. This is destructive - any saved creature will be overwritten."
+        );
+
+		const importAdditional =
+			importSettingsContainer.createDiv("additional");
+
+		const importAppFile = new Setting(importAdditional)
+            .setName("Import DnDAppFile")
+            .setDesc("Only import content that you own.");
+
+		const inputAppFile = createEl("input", {
+			attr: {
+				type: "file",
+				name: "dndappfile",
+				accept: ".xml",
+				multiple: true
+			}
+		});
+
+		inputAppFile.onchange = async () => {
+            const { files } = inputAppFile;
+            if (!files.length) return;
+            try {
+                const { files } = inputAppFile;
+                if (!files.length) return;
+                const monsters = await this.importer.import(files, "appfile");
+                if (monsters && monsters.length) {
+                    await this.plugin.saveMonsters(monsters);
+                }
+                this.display();
+            } catch (e) {}
+        };
+
+		importAppFile.addButton((b) => {
+            b.setButtonText("Choose File(s)").setTooltip(
+                "Import DnDAppFile Data"
+            );
+            b.buttonEl.addClass("statblock-file-upload");
+            b.buttonEl.appendChild(inputAppFile);
+            b.onClick(() => inputAppFile.click());
+        });
 	}
 }
